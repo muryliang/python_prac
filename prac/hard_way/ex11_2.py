b63f7c73254093d595cf220ae3b325eb8360b887
muryliang <muryliang@gmail.com>
2017-03-14 15:06:48 +0800

    udpate

---
+++ prac/hard_way/ex11_2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+print "what's your favourite singer?",
+singer = raw_input()
+print "where do you live?",
+where = raw_input()
+print "I see... your favourite singer is %s, you live in %s"%(singer, where)
