829cf2d02d667951e91371d52a911e5025ab148c
muryliang <muryliang@gmail.com>
2017-04-14 09:54:08 +0800

    update

--- tensorflow/mmist.py
+++ tensorflow/mmist.py
@@ -5,6 +5,9 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
 
 x = tf.placeholder(tf.float32, [None, 784]) #一个占位符，第一维度可以无线延伸
 W = tf.Variable(tf.zeros([784, 10]))
+#在这里面，这个Ｗ就是一个，筛子，每一列代表对应数字的筛子，每次都是一个７４８像素的ｘ的输入
+#和１０列这样的筛子得出１０个结果，可以理解为１０个向量的相似程度，越相似，就越代表几率高，
+#这个几率由ｓｏｆｔｍａｘ来整合得到,最后拿几率最高的出去比较，代表就是这个数，求概率
 b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder("float", [None, 10])
