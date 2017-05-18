a4f58c72e8ea850c7235afaf653fb1eaffdd29b8
muryliang <muryliang@gmail.com>
2017-05-18 16:08:01 +0800

    update

---
+++ prac/fish_crawl_now.py
@@ -0,0 +1,69 @@
+from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup
+from lxml.html import fromstring
+import re
+import time
+import string
+import os
+
+clist = dict()
+count = 0
+cset = set()
+headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0', 'Accept-Encoding':'gzip, deflate', 'Accept-Language':'en-US,en;q=0.5', 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+
+for pattern in string.ascii_lowercase:
+    start_url = 'http://site1.zjou.edu.cn/fish/fish_1.asp?px=id&PageNo=1&typer=yw&key='+pattern+'&liker=0&page=5000&order=ASC'
+    print ("start searching pattern", pattern)
+    start_page = requests.get(start_url, headers = headers)
+    start_page.encoding = 'gbk'
+    tree = fromstring(start_page.text)
+    nameurls = tree.xpath("//span[@class='style5']/a/@onclick")
+    names = tree.xpath("//span[@class='style5']/a/text()")
+    #学名这个有漏的，不能这样匹配，会移位，只能在里面找
+    #scinames = tree.xpath("//span[@class='style5']/i/text()")
+    print (len(names), len(nameurls))
+    for name, nameurl in zip(names, nameurls):
+        if nameurl in cset:
+            continue
+        cset.add(nameurl)
+        parturl = re.match("MM_openBrWindow\('(fish[^']*).*", nameurl).group(1) 
+        wholeurl = urljoin(start_url, parturl)
+        infopage = requests.get(wholeurl, headers = headers)
+        # 网页的charset里是gb2312，但是实际使用发现gbk才能保证没有乱码字符,gb2312偶尔会乱码某些字符，如"鱚"不行
+        infopage.encoding = 'gbk'
+        infoobj = BeautifulSoup(infopage.text, 'html.parser')
+        tbl = infoobj.body.table.findAll("tr")[1].td.findAll("tr") 
+        imgs = tbl[5]
+        tbl = tbl[:5]
+        clist[name] = dict()
+        print ("processing:", tbl[0].td.font.b.contents)
+        clist[name]['中文名'] = name
+        sciname = tree.xpath("//span[@class='style5']/i[../a/text()=\'"+name+"\']/text()")
+        print ("sciname is", sciname)
+        if len(sciname) == 0:
+            clist[name]['学名'] = tbl[0].td.font.b.em.get_text()
+        else :
+            clist[name]['学名'] = sciname[0]
+    # 不同物种里的结构不一样，有些不能用contents分，需要正则结合之前的学名来获取，价值不大，先不弄
+    #    clist[name]['recorder'] = "".join([str(k).strip('</em>') for k in tbl[0].td.font.b.contents[2:]])
+
+        for i in range(1,4):
+            clist[name][tbl[i].findAll("td")[0].get_text().strip().replace('\xa0','')] = \
+                tbl[i].findAll("td")[1].get_text().strip().replace('\xa0','')
+        print ("after processing:", clist[name])
+
+        clist[name]['描述'] = tbl[4].findAll("td")[0].get_text().strip().replace('\xa0','')
+
+        for imgsrc in imgs.findAll("img"):
+            imgurl = urljoin(start_url, imgsrc['src'])
+            postfix = imgurl.split('/')[-1]
+            imgcontent = requests.get(imgurl, headers= headers)
+            clist[name][postfix] = imgcontent.content
+            filename = os.path.join("/tmp/pic", postfix)
+            with open(filename, "wb+") as f:
+                f.write(imgcontent.content)
+        print ("collected", name, clist[name].keys())
+        count = count + 1
+        time.sleep(0.1)
+print ("count is", count)
--- prac/shell_crawl.py
+++ prac/shell_crawl.py
@@ -2,13 +2,14 @@ from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 from lxml.html import fromstring
+import os
 
 start_url = 'http://site1.zjou.edu.cn/fish/shell_1.asp?px=id&lx=%CE%DE%B0%E5%B8%D9%2C+%B5%A5%B0%E5%B8%D9%2C+%B6%E0%B0%E5%B8%D9%2C+%B8%B9%D7%E3%B8%D9%2C+%BE%F2%D7%E3%B8%D9%2C+%CB%AB%BF%C7%B8%D9%2C+%CD%B7%D7%E3%B8%D9&PageNo=1&typer=0&key=&liker=1&page=40&order=ASC'
 
 headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0', 'Accept-Encoding':'gzip, deflate', 'Accept-Language':'en-US,en;q=0.5', 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
 
 start_page = requests.get(start_url, headers = headers)
-start_page.encoding = 'gb2312'
+start_page.encoding = 'gbk'
 #bsobj = BeautifulSoup(start_page.text, 'html.parser')
 #bsobj.body
 tree = fromstring(start_page.text)
@@ -19,7 +20,7 @@ count = 0
 for nameurl in nameurls:
         shellurl = urljoin(start_url, nameurl)
         page_response = requests.get(shellurl, headers = headers)
-        page_response.encoding = 'gb2312'
+        page_response.encoding = 'gbk'
         bsobj = BeautifulSoup(page_response.text, 'html.parser')
         tbl = bsobj.body.findAll("table")[1].findAll("td")
         imgs = tbl[2:4]
@@ -32,7 +33,11 @@ for nameurl in nameurls:
             join_link = urljoin(start_url, link)
             key_name = link.split('/')[-1]
             pic_content = requests.get(join_link).content
-            clist[name][key_name] = pic_content
+            #### store pictures in to file
+#            filename = os.path.join("/tmp/pic", key_name)
+#            with open(filename, "wb+") as f:
+#                f.write(pic_content)
+#            clist[name][key_name] = pic_content
         print ("collected!!: ", name, clist[name].keys())
         count = count + 1
 print ("all ", count , "items")
