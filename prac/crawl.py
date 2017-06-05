44d049793ce6eb3b0fe8091620df58fcb3ad765c
muryliang <muryliang@gmail.com>
2017-06-05 17:07:29 +0800

    update

--- prac/crawl.py
+++
@@ -1,5 +0,0 @@
-#-*- coding: utf-8 -*-
-from icrawler.builtin import GoogleImageCrawler, BaiduImageCrawler
-
-baidu = GoogleImageCrawler(storage={'root_dir':'/tmp/ac'}, downloader_threads=8)
-baidu.crawl(keyword='super', offset=0, max_num=1000, min_size=None, max_size=None)
