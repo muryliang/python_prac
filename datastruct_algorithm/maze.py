ebbe02efab8a04b425f2eb9663d3b1de091943e0
muryliang <muryliang@gmail.com>
2017-08-10 14:25:19 +0800

    update

--- datastruct_algorithm/maze.py
+++ datastruct_algorithm/maze.py
@@ -1,15 +1,18 @@
 import turtle
 import random
+import pprint
 
 class route():
     """draw the whole map"""
 
-    def __init__(self, rate=0.5, color='brown', backcolor='white', pensize=3, blks=10):
+    def __init__(self, rate=0.5, color='brown', backcolor='white', pensize=3, blks=10, dest = (0,0)):
+        self.dest = dest
         self.blks = blks
         self.pensize = pensize
         self.color = color
         self.backcolor = backcolor
         self.rate = rate
+        self.map = list()
 
         self.turtle = turtle.Turtle()
         t = self.turtle
@@ -20,33 +23,38 @@ class route():
         t.pensize(pensize)
 
         # draw every blocks with rate
-        for i, j in ((x,y) for x in range(blks) for y in range(blks)):
-            t.up()
-            t.goto(j * self.unitw - 320, i * self.unith - 300)
-            if random.random() <= rate:
-                t.fillcolor(color)
-            else:
-                t.fillcolor(backcolor)
-            t.down()
-            t.begin_fill()
-            t.seth(90)
-            t.forward(self.unitw)
-            t.right(90)
-            t.forward(self.unith)
-            t.right(90)
-            t.forward(self.unitw)
-            t.right(90)
-            t.forward(self.unith)
-            t.end_fill()
+#        for i, j in ((x,y) for x in range(blks) for y in range(blks)):
+        for i in range(blks):
+            self.map.append(list())
+            for j in range(blks):
+                t.up()
+                t.goto(i * self.unitw - 320, j * self.unith - 300)
+                if random.random() > rate or (i, j) == (0, 0) or (i, j) == dest:
+                    t.fillcolor(backcolor)
+                    self.map[-1].append(['empty', 0]) # 0 means not visited yet
+                else:
+                    t.fillcolor(color)
+                    self.map[-1].append(['block', 0])
+                t.down()
+                t.begin_fill()
+                t.seth(90)
+                t.forward(self.unitw)
+                t.right(90)
+                t.forward(self.unith)
+                t.right(90)
+                t.forward(self.unitw)
+                t.right(90)
+                t.forward(self.unith)
+                t.end_fill()
 
     def setPos(self, x, y):
         """set cursor's position"""
 
         t = self.turtle
-        self.x = x * self.unitw - 320;
-        self.y = y * self.unith - 300;
+        self.x = x 
+        self.y = y
         t.up()
-        t.goto(self.x, self.y)
+        t.goto(x * self.unitw - 320, y * self.unith - 300)
         t.down()
 
     def drawCircle(self, color='black'):
@@ -65,6 +73,24 @@ class route():
         t.circle(min(self.unitw/4, self.unith/4))
         t.end_fill()
 
+    def isDest(self):
+        """if is destination, return ture"""
+
+        if not self.available(self.x, self.y):
+            return False
+        if self.dest == (self.x, self.y):
+            print ("found dest!!")
+            return True
+
+    def available(self, x, y):
+        """whether current position can be forwarded"""
+
+        if x < 0 or x >= self.blks or y < 0 or y >=self.blks:
+            return False
+        if self.map[x][y][0] == 'block' or self.map[x][y][1] == 1:
+            return False
+        return True
+
     def start(self):
         """finding and color the path
 
@@ -74,14 +100,50 @@ class route():
         if get to exit pot, return true, then recursively mark
         points on that path green
         """
+        if self.search(self.x, self.y):
+            print ("found a way")
+        else:
+            print ("not found")
 
-        x = self.x
-        y = self.y
+    def search(self, x, y):
+        """this function do recursive search"""
+
+        assert self.map[x][y][0] != 'block' and self.map[x][y][1] != 1
+        self.turtle.up()
+        self.setPos(x, y)
+        self.turtle.down()
+        if self.isDest(): # if is destination, return true
+            self.drawCircle(color='green')
+            return True
+        else:
+            self.drawCircle(color='black')
+        self.map[x][y][1] = 1 #mark visited
+
+        res = False
+        if not res and self.available(x, y+1):
+            res = self.search(x, y+1)
+        if not res and self.available(x, y-1):
+            res = self.search(x, y-1)
+        if not res and self.available(x-1, y):
+            res = self.search(x-1, y)
+        if not res and self.available(x+1, y):
+            res = self.search(x+1, y)
+
+        self.turtle.up()
+        self.setPos(x, y)
+        self.turtle.down()
+        if res:
+            self.drawCircle(color='green')
+        else:
+            self.drawCircle(color='red')
+        return res
         
-blks = 5
-rate = 0.2
+blks = 10
+rate = 0.3
 turtle.delay(1)
-r = route(rate = rate, blks = blks)
+r = route(rate = rate, blks = blks, dest = (8,7))
+pprint.pprint(r.map)
+#turtle.delay(10)
 r.setPos(0,0)
-r.drawCircle()
+r.start()
 r.screen.exitonclick()
