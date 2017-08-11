0f1a4c77d3faccef9e165dac4457f28a701a211b
muryliang <muryliang@gmail.com>
2017-08-11 17:38:39 +0800

    update

--- datastruct_algorithm/hano.py
+++ datastruct_algorithm/hano.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import turtle
 
 def move(num, start, end, mid):
     """move from start to end, use mid to tmp store"""
@@ -8,18 +9,66 @@ def move(num, start, end, mid):
     if num == 0:
         return
     move(num-1, start, mid, end)
-    do_move(start, end)
+    do_move(start, end, num)
     count += 1
     move(num-1, mid, end, start)
 
-def do_move(start, end):
-#    print ("move from %d to %d"%(start, end))
-    pass
+def do_move(start, end, num):
+    global s
+    print ("move panel %d from %d to %d"%(num, start, end))
+    s[end-1].append(s[start-1].pop())
+    redraw(s)
+
+def redraw(s):
+    global t
+    height = -200
+    t.clear()
+    start = -200
+    t.up()
+    t.goto(start, height)
+    for i in range(3):
+        turtle.tracer(100)
+        for length in s[i]:
+            drawTangle(length)
+        t.up()
+        t.goto(start + (i+1)*200, height)
+        turtle.tracer(1)
+        time.sleep(0.5)
+
+def drawTangle(length):
+    global t
+    length *= 1
+    width=10
+    t.down()
+    t.seth(180)
+    t.fd(length/2)
+    t.seth(90)
+    t.fd(width)
+    t.seth(0)
+    t.fd(length)
+    t.seth(270)
+    t.fd(width)
+    t.seth(180)
+    t.fd(length/2)
+    t.up()
+    t.seth(90)
+    t.fd(width)
+
+s = list()
+for _ in range(3):
+    s.append(list())
+
+turtle.delay(5)
+t = turtle.Turtle()
+scr = turtle.Screen()
+
+numbers = int(sys.argv[1])
+for i in range(numbers, 0, -1):
+    s[0].append(i)
 
 count = 0
-numbers = int(sys.argv[1])
-#numbers = int(input("input your panels: "))
 tick = time.time()
 move(numbers, 1, 3, 2)
 print ("all cost %d steps, time %f"%(count, time.time() - tick))
+scr.exitonclick()
 
