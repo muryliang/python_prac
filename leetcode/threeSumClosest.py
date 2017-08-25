00556a80381b609c48956d254013fe011dc83dcb
muryliang <muryliang@gmail.com>
2017-08-25 23:24:08 +0800

    udpate

--- leetcode/threeSumClosest.py
+++ leetcode/threeSumClosest.py
@@ -1,40 +1,43 @@
-def twoSumClosest(nums, target, pos):
-    lst = nums
+import sys
+
+def twoSumClosest(nums, target, elem, pos):
     start = pos
     end = len(nums)-1
-    reslist = []
+    least = sys.maxsize
+    res = sys.maxsize
     while start < end:
-        res = target - nums[start]
-        if res > nums[end]:
+        diff = elem + nums[start] + nums[end]
+        if diff - target < 0:
+            if target - diff < res:
+                least = diff
+                res = target - diff
             start += 1
-        elif res < nums[end]:
+        elif diff - target > 0:
+            if diff - target < res:
+                least = diff
+                res = diff - target
             end -= 1
         else:
-            reslist.append([nums[start], nums[end]])
-            while end -1 > start and nums[end-1] == nums[end]:
-                end -= 1
-            end -= 1
-            while start + 1 < end and nums[start+1] == nums[start]:
-                start += 1
-            start += 1
-    return reslist
+            return diff
 
-def threeSumClosest(nums):
+    return least
+
+def threeSumClosest(nums, target):
     """method: like three, first find twoSumClosest, but return two list for 
         every element, one positive least pair, one negative pair
         the outer loop update the least pair every time
     """
     nums.sort()
-    reslist = list()
+    least = sys.maxsize # a very big number
     for idx, elem in enumerate(nums):
-        if idx == 0 or elem != nums[idx-1]: #forbid duplicate
-            sublist = twoSum(nums, 0-elem, idx+1)
-            for lst in sublist:
-                reslist.append([elem, lst[0], lst[1]])
-    return reslist
+        tmpresult = twoSumClosest(nums, target, elem, idx+1)
+        if abs(least - target) > abs(tmpresult - target):
+            least = tmpresult
+    return least
 
     
 
 if __name__ == "__main__":
-    lst = [-1, 0, 1, 2, -1, -4]
-    print (threeSum2(lst))
+    lst = [0,1,2]
+    target = 3
+    print (threeSumClosest(lst, target))
