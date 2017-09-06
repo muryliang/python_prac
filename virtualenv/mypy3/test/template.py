57d561b8d64b2dfd92a9eedd0d42d0a1b86db783
muryliang <muryliang@gmail.com>
2017-09-06 17:38:55 +0800

    update

--- virtualenv/mypy3/test/template.py
+++ virtualenv/mypy3/test/template.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 # Foundations of Python Network Programming, Third Edition
-# https://github.com/brandom-rhodes/fopnp/blob/mn/py3/chapter01/search2.py
+# https://github.com/brandom-rhodes/fopnp/blob/mn/py3/chapter02/udp_local.py
