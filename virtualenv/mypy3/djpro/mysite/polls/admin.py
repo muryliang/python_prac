0301e8f523a2e31e417fd99a968ad8414e9a1e08
muryliang <muryliang@gmail.com>
2017-09-18 12:13:27 +0800

    udpate

--- virtualenv/mypy3/djpro/mysite/polls/admin.py
+++ virtualenv/mypy3/djpro/mysite/polls/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- virtualenv/mypy3/djpro/mysite/polls/views.py
+++ virtualenv/mypy3/djpro/mysite/polls/views.py
@@ -13,7 +13,6 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-#        return Question.objects.order_by('-pub_date')[:5]
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
